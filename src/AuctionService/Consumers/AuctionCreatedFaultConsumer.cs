112277044a7d6d1866da02a50e905c3d2d29e769
Dayo Okesola <dayo@flutterwavego.com>
2024-09-28 17:37:33 +0100

    completed section 6

--- src/AuctionService/Consumers/AuctionCreatedFaultConsumer.cs
+++ src/AuctionService/Consumers/AuctionCreatedFaultConsumer.cs
@@ -9,7 +9,7 @@ public class AuctionCreatedFaultConsumer : IConsumer<Fault<AuctionCreated>>
     public async Task Consume(ConsumeContext<Fault<AuctionCreated>> context)
     {
         var exception = context.Message.Exceptions.First();
-        if(exception.ExceptionType == "System.ArgumentException")
+        if (exception.ExceptionType == "System.ArgumentException")
         {
             context.Message.Message.Model = "FooBar";
             await context.Publish(context.Message.Message);
--- src/Contracts/AuctionUpdated.cs
+++ src/Contracts/AuctionUpdated.cs
@@ -4,12 +4,8 @@ public class AuctionUpdated
 {
     public string Id { get; set; }
     public string Make { get; set; }
- 
     public string Model { get; set; }
- 
     public int Year { get; set; }
- 
     public string Color { get; set; }
- 
-    public int Mileage { get; set; } 
+    public int Mileage { get; set; }
 }
